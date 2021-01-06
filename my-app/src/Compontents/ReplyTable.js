b59fe0b73eeff712557898321e3641b16778f07e
Beniamin Terza <beniaminterza@gmail.com>
2021-01-06 17:16:10 +0100

    table fixed

--- my-app/src/Compontents/ReplyTable.js
+++ my-app/src/Compontents/ReplyTable.js
@@ -1,24 +1,27 @@
 import React from "react";
 
-export default function ReplyTable({ tableContent}) {
+export default function ReplyTable({ tableContent }) {
     return (
         <div className="contentContainer table">
             <h3>Reply</h3>
             <table>
-                <tr className="header">
-                    <th>
-                        <p>IP Address</p>
-                    </th>
-                    <th>
-                        <p>OID</p>
-                    </th>
-                    <th>
-                        <p>Type</p>
-                    </th>
-                    <th>
-                        <p>Value</p>
-                    </th>
-                </tr>
+                <thead>
+                    <tr className="header">
+                        <th>
+                            <p>IP Address</p>
+                        </th>
+                        <th>
+                            <p>OID</p>
+                        </th>
+                        <th>
+                            <p>Type</p>
+                        </th>
+                        <th>
+                            <p>Value</p>
+                        </th>
+                    </tr>
+                </thead>
+
                 {tableContent}
             </table>
         </div>
--- my-app/src/Compontents/ReplyTableElement.js
+++ my-app/src/Compontents/ReplyTableElement.js
@@ -2,19 +2,21 @@ import React from "react";
 
 export default function ReplyTableElement({ ip, oid, type, value }) {
     return (
-        <tr className={type.toLowerCase()}>
-            <td>
-                <p>{ip}</p>
-            </td>
-            <td>
-                <p>{oid}</p>
-            </td>
-            <td>
-                <p>{type}</p>
-            </td>
-            <td>
-                <p>{value}</p>
-            </td>
-        </tr>
+        <tbody>
+            <tr className={type.toLowerCase()}>
+                <td>
+                    <p>{ip}</p>
+                </td>
+                <td>
+                    <p>{oid}</p>
+                </td>
+                <td>
+                    <p>{type}</p>
+                </td>
+                <td>
+                    <p>{value}</p>
+                </td>
+            </tr>
+        </tbody>
     );
 }
